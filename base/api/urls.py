b7977dad1c8d07cd4fde94b72b1e87ad721f2164
Jose Masri <josemasri2@gmail.com>
2022-10-22 15:47:24 -0500

    initial commit

---
+++ base/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import MyTokenObtainPairView, getRoutes
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+
+urlpatterns = [
+    path('', getRoutes),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
